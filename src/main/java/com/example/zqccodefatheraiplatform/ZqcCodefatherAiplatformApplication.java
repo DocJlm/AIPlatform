f9762b7666d5c8a3026361e89a8f6fe28673b35a
DOC.JLM <1952199902@qq.com>
2025-09-30 21:43:55 +0800

    添加用户模块基础代码

--- src/main/java/com/example/zqccodefatheraiplatform/ZqcCodefatherAiplatformApplication.java
+++ src/main/java/com/example/zqccodefatheraiplatform/ZqcCodefatherAiplatformApplication.java
@@ -1,9 +1,11 @@
 package com.example.zqccodefatheraiplatform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.zqccodefatheraiplatform.mapper")
 public class ZqcCodefatherAiplatformApplication {
 
     public static void main(String[] args) {
