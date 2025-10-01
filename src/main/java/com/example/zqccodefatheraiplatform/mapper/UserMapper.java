09d604361e00ea18dd15741d00d7030f867fa103
DOC.JLM <1952199902@qq.com>
2025-10-01 12:22:13 +0800

    用户登录后端

---
+++ src/main/java/com/example/zqccodefatheraiplatform/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.zqccodefatheraiplatform.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.example.zqccodefatheraiplatform.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/DocJlm/DocJlm">DocJlm</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
